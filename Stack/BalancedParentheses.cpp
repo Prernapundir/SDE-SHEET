7bdbf2069c5a21fc8b9ed494fa72a08ae2e3f208
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2021-12-24 21:23:19 +0530

    Create BalancedParentheses.cpp

---
+++ Stack/BalancedParentheses.cpp
@@ -0,0 +1,29 @@
+/*Approach
+1.Simple implementation using stack
+*/
+
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int i=0;
+        stack<char> st;
+        
+        while(i<s.size()){
+            if(s[i]=='(' or s[i]=='{' or s[i]=='['){
+                st.push(s[i]);
+            }else if(!st.empty() and s[i]==')' and st.top()=='('){
+                st.pop();
+            }else if(!st.empty() and s[i]=='}' and st.top()=='{'){
+                st.pop();
+            }else if(!st.empty() and s[i]==']' and st.top()=='['){
+                st.pop();
+            }else{
+                return false;
+            }
+            i++;
+        }
+        return st.size()==0 and i>=s.size();
+    }
+};
