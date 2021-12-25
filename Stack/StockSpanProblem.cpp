ac90dbf5cae7b570f24245937c8009b72a3a9337
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2021-12-25 16:58:27 +0530

    Update StockSpanProblem.cpp

--- Stack/StockSpanProblem.cpp
+++ Stack/StockSpanProblem.cpp
@@ -4,29 +4,30 @@
 3. In a stack store nearest graeter element
 */
 
+
 class Solution
 {
     public:
     //Function to calculate the span of stockâ€™s price for all n days.
     vector <int> calculateSpan(int price[], int n)
     {
-       // Your code here
-       vector<int> ans(n,1);
-       stack<pair<int,int>>st;
-       st.push({price[0],0});
        
-       for(int i=1;i<n;i++){
-           while(!st.empty() and st.top().first<=price[i]){
+       vector<int> ans(n,1);
+       stack<int>st;
+    
+       for(int i=0;i<n;i++){
+           while(!st.empty() and price[st.top()]<=price[i]){
                st.pop();
            }
            if(!st.empty()){
-               ans[i]=i-st.top().second;
+               ans[i]=i-st.top();
            }else{
                ans[i]=i+1;
            }
-           st.push({price[i],i});
+           st.push(i);
        }
        
        return ans;
     }
 };
+
