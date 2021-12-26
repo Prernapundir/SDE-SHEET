58e6f66af4a90fc57689b708af7b656e9e40ceb9
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2021-12-26 21:42:24 +0530

    Create RotateMatrix.cpp

---
+++ Day2/RotateMatrix.cpp
@@ -0,0 +1,24 @@
+/*
+Step1: Transpose of the matrix. (transposing means changing columns to rows and rows to columns)
+Step2: Reverse each row of the matrix.
+*/
+
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        
+        
+        for(int i=0;i<m;i++){
+            for(int j=i+1;j<m;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<m;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        return;
+    }
+};
