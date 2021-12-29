e60551b5b5a19736aa2505a23f2e98805592bffe
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2021-12-29 17:38:19 +0530

    Create NthRootOfInterger.cpp

---
+++ Day11/NthRootOfInterger.cpp
@@ -0,0 +1,24 @@
+/*
+1.Use Binary Search
+2. Take eps value 10^-6, as we are calculating decimal upto 6 places
+*/
+
+
+void getNthRoot(int n, int m) {
+    double low = 1;
+    double high = m;
+    double eps = 1e-6; 
+    
+    while((high - low) > eps) {
+        double mid = (low + high) / 2.0; 
+        if(multiply(mid, n) < m) {
+            low = mid; 
+        }
+        else {
+            high = mid; 
+        }
+    }
+    
+    cout <<n<<"th root of "<<m<<" is "<<low<<endl; 
+    
+}
