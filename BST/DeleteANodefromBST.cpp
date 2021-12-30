935125bdeea2ab6b2fa1b12f22ae11a24e775984
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2021-12-30 21:15:41 +0530

    Update DeleteANodefromBST.cpp

--- BST/DeleteANodefromBST.cpp
+++ BST/DeleteANodefromBST.cpp
@@ -9,19 +9,25 @@ public:
         }else if(root->val<key){
             root->right=deleteNode(root->right,key);
         }else{
+            // No child condition
             if(!root->left and !root->right) {
                 delete root;
                 return NULL;
-            }else if(!root->left or !root->right){
+            }
+            // One child condition
+            else if(!root->left or !root->right){
                 TreeNode* temp=root->left?root->left:root->right;
                 delete(root);
                 return temp;
-            }else{
+            }
+            // If both left and right child exists {either find right min node or find left greater node}
+            else{
                 TreeNode* temp=root->right;
                 while(temp->left){
                     temp=temp->left;
                 }
                 root->val=temp->val;
+                // now our work reduce to delete the temp node
                 root->right=deleteNode(root->right,temp->val);
                 return root;
             }
