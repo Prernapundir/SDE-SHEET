198c537d8441be04ffb03cca603c0cdb3b88d59c
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2022-01-02 16:39:41 +0530

    Update Detect Cycle In Directed Graph.cpp

--- Graph/Detect Cycle In Directed Graph.cpp
+++ Graph/Detect Cycle In Directed Graph.cpp
@@ -36,3 +36,58 @@ public:
 	   	return false;
 	}
 };
+
+
+
+/*BFS using Topological Sort [Kahn's Algorithm}
+==> If able to generate topological sort , then cycle not present , else present
+
+*/
+
+
+class Solution {
+public:
+    
+    
+    bool canFinish(int n, vector<vector<int>>& a) {
+        
+        vector<int> adj[n];
+        for(auto i:a){
+           adj[i[0]].push_back(i[1]); 
+        }
+        
+        queue<int> q;
+        vector<bool> vis(n);
+        int count=0;
+        vector<int> indegree(n,0);
+        
+        for(int i=0;i<n;i++){
+            for(auto x:adj[i]){
+                indegree[x]++;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int f=q.front();
+            q.pop();
+            count++;
+            
+            for(auto x:adj[f]){
+                indegree[x]--;
+                if(indegree[x]==0){
+                    q.push(x);
+                }
+            }
+        }
+        if(count==n) return true;
+        return false;
+        
+    }
+};
+
+
+
